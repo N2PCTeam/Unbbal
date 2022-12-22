50b0bff95640228d1189fda35909f57c472f1232
john9590 <john9590@naver.com>
2022-12-22 22:49:49 +0900

    add Miracle

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ count = 1000
 date = "2022-12-18"
 cursor = ""
 rv = stats.norm(0,1)
-s = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-t = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-c = ["수상한 큐브","레드 큐브","블랙 큐브","장인의 큐브","명장의 큐브"]
-o = ["레어","에픽","유니크","레전드리"]
-cp = [[0.9901,6,15,4.7619,7.9994],[0,1.8,3.5,1.1858,1.6959],[0,0.3,1.2,0,0.1996]]
+s = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+t = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+m = [1,3,3,2,3]
+c = ["수상한 큐브","레드 큐브","블랙 큐브","장인의 큐브","명장의 큐브","수상한 큐브","레드 큐브","블랙 큐브","장인의 큐브","명장의 큐브"]
+o = ["레어","에픽","유니크","레전드리","(미)에픽","(미)유니크","(미)레전드리"]
+cp = [[0.9901,6,15,4.7619,7.9994],[0,1.8,3.5,1.1858,1.6959],[0,0.3,1.2,0,0.1996],[0.9901*2,6*2,15*2,4.7619*2,7.9994*2],[0*2,1.8*2,3.5*2,1.1858*2,1.6959*2],[0*2,0.3*2,1.2*2,0*2,0.1996*2]]
 sdate = input()
 fdate = input()
 sdate = dt.datetime.strptime(sdate,"%Y%m%d")
@@ -34,52 +35,20 @@ while fdate>=sdate:
         if len(data) == 2 :
             break
         for i in range(0,data['count']):
-            suc = data['cube_histories'][i]['item_upgrade_result']
             cube = data['cube_histories'][i]['cube_type']
             potential = data['cube_histories'][i]['before_potential_options'][0]['grade']
             ds = 0
-            if suc == "성공":
+            if data['cube_histories'][i]['item_upgrade_result'] == "성공":
                 ds = 1
-            if cube == c[0] and potential == "레어":
-                t[0][0]+=1
-                s[0][0]+=ds
-            if cube == c[1]:
-                if potential == o[0]:
-                    t[0][1]+=1
-                    s[0][1]+=ds
-                if potential == o[1]:
-                    t[1][1]+=1
-                    s[1][1]+=ds
-                if potential == o[2]:
-                    t[2][1]+=1
-                    s[2][1]+=ds
-            if cube == c[2]:
-                if potential == o[0]:
-                    t[0][2]+=1
-                    s[0][2]+=ds
-                if potential == o[1]:
-                    t[1][2]+=1
-                    s[1][2]+=ds
-                if potential == o[2]:
-                    t[2][2]+=1
-                    s[2][2]+=ds
-            if cube == c[3]:
-                if potential == o[0]:
-                    t[0][3]+=1
-                    s[0][3]+=ds
-                if potential == o[1]:
-                    t[1][3]+=1
-                    s[1][3]+=ds
-            if cube == c[4]:
-                if potential == o[0]:
-                    t[0][4]+=1
-                    s[0][4]+=ds
-                if potential == o[1]:
-                    t[1][4]+=1
-                    s[1][4]+=ds
-                if potential == o[2]:
-                    t[2][4]+=1
-                    s[2][4]+=ds
+            dj = 0
+            if data['cube_histories'][i]['miracle_time_flag'] != "이벤트 적용되지 않음":
+                dj = 4
+            for j in range(0,5):
+                if cube == c[j]:
+                    for k in range(0,m[j]):
+                        if potential == o[k]:
+                            t[k][j+dj]+=1
+                            s[k][j+dj]+=ds
         if data['next_cursor'] == '':
             break
         else :
@@ -93,7 +62,7 @@ result = 0
 tsum = 0
 for i in range(0,5):
     print(c[i])
-    for j in range(0,3):
+    for j in range(0,6):
         if t[j][i] > 0 :
             print(o[j+1])
             fun=stats.norm(cp[j][i]*t[j][i]/100,(cp[j][i]*t[j][i]/100*(1-cp[j][i]/100))**(1/2))
