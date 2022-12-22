01faccc291dc8fc7739f1495126fd1d55cf11d07
john9590 <john9590@naver.com>
2022-12-23 03:12:22 +0900

    add potential

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime as dt
 import math
 import scipy.stats as stats
 
-api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjIwOTc0NzA1NDciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4NzA2NjI4MiwiaWF0IjoxNjcxNTE0MjgyLCJuYmYiOjE2NzE1MTQyODIsInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.Oq2FtkLydQBlNAg3p8RXCTszi_SQ1HEvpNhZky9_XXk"
+api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE3NDUzMzQ1MzMiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4NzI2NTIyOSwiaWF0IjoxNjcxNzEzMjI5LCJuYmYiOjE2NzE3MTMyMjksInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.0RhrmXdAqXJLyiKU51XvmSpASpanZmbkkPaQ4-GzGeg"
 count = 1000
 date = "2022-12-18"
 cursor = ""
@@ -15,7 +15,17 @@ t = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 m = [1,3,3,2,3]
 c = ["수상한 큐브","레드 큐브","블랙 큐브","장인의 큐브","명장의 큐브","수상한 큐브","레드 큐브","블랙 큐브","장인의 큐브","명장의 큐브"]
 o = ["레어","에픽","유니크","레전드리","(미)에픽","(미)유니크","(미)레전드리"]
-cp = [[0.9901,6,15,4.7619,7.9994],[0,1.8,3.5,1.1858,1.6959],[0,0.3,1.2,0,0.1996],[0.9901*2,6*2,15*2,4.7619*2,7.9994*2],[0*2,1.8*2,3.5*2,1.1858*2,1.6959*2],[0*2,0.3*2,1.2*2,0*2,0.1996*2]]
+e = ["무기","엠블렘","보조무기", "포스실드","소울링","방패","모자","상의","한벌옷","하의","신발","장갑","망토","벨트","어깨장식","얼굴장식","눈장식","귀고리","반지","펜던트","기계심장"]
+u = ["보스","방어율","공격력","마력","STR","DEX","INT","LUK","올스탯","모든 스킬의","크리티컬 데미지"]
+#       0      1       2        3     4      5    6     7      8       9             10
+eu = [[[0,1,2],[1,2],[0,1,2],[0,1,2],[0,1,2],[0,1,2],[4,8,9],[4,8],[4,8],[4,8],[4,8],[4,8,10],[4,8],[4,8],[4,8],[4,8],[4,8],[4,8],[4,8],[4,8],[4,8]],
+[[0,1,2],[1,2],[0,1,2],[0,1,2],[0,1,2],[0,1,2],[5,8,9],[5,8],[5,8],[5,8],[5,8],[5,8,10],[5,8],[5,8],[5,8],[5,8],[5,8],[5,8],[5,8],[5,8],[5,8]],
+[[0,1,2],[1,2],[0,1,2],[0,1,2],[0,1,2],[0,1,2],[7,8,9],[7,8],[7,8],[7,8],[7,8],[7,8,10],[7,8],[7,8],[7,8],[7,8],[7,8],[7,8],[7,8],[7,8],[7,8]],
+[[0,1,3],[1,3],[0,1,3],[0,1,3],[0,1,3],[0,1,3],[6,8,9],[6,8],[6,8],[6,8],[6,8],[6,8,10],[6,8],[6,8],[6,8],[6,8],[6,8],[6,8],[6,8],[6,8],[6,8]]]
+ep = [[1.2090,0.3238,0.6637,0.6637,0.6637,0.1593,0.6551,0.2922,0.2922,0.4711,0.4333,0.5382,0.5725,0.5725,0.5725,0.6658,0.6658,0.6658,0.6658,0.6658,0.9556]]
+es = [[0 for i in range(len(e))],[0 for i in range(len(e))]]
+et = [[0 for i in range(len(e))],[0 for i in range(len(e))]]
+cp = [[0.9901,6,15,4.7619,7.9994],[0,1.8,3.5,1.1858,1.6959],[0,0.3,1.2,0,0.1996],[0.9901*2,6*2,15*2,4.7619*2,7.9994*2],[0*2,1.8*2,3.5*2,1.1858*2,1.6959*2],[0*2,0.3*2,1.2*2,0*2,0.1996*2],[]]
 sdate = input()
 fdate = input()
 sdate = dt.datetime.strptime(sdate,"%Y%m%d")
@@ -38,9 +48,9 @@ while fdate>=sdate:
             cube = data['cube_histories'][i]['cube_type']
             potential = data['cube_histories'][i]['before_potential_options'][0]['grade']
             ds = 0
+            dj = 0
             if data['cube_histories'][i]['item_upgrade_result'] == "성공":
                 ds = 1
-            dj = 0
             if data['cube_histories'][i]['miracle_time_flag'] != "이벤트 적용되지 않음":
                 dj = 4
             for j in range(0,5):
@@ -49,6 +59,31 @@ while fdate>=sdate:
                         if potential == o[k]:
                             t[k][j+dj]+=1
                             s[k][j+dj]+=ds
+            for k in range(0,1):
+                if cube == c[k] and data['cube_histories'][i]['potential_option_grade'] == o[3]:
+                    for j in range(0,len(e)):
+                        if data['cube_histories'][i]['item_equip_part'] == e[j] :
+                            et[k][j] += 1
+                            item = data['cube_histories'][i]['target_item']
+                            job = 0
+                            if item.find("워리어") >= 0 or item.find("나이트") >= 0:
+                                job = 0
+                            if item.find("레인져") >= 0 or item.find("아처") >= 0 :
+                                job = 1
+                            if item.find("어새신") >= 0 or item.find("시프") >= 0 :
+                                job = 2
+                            if item.find("던위치") >= 0 or item.find("메이지") >= 0 :
+                                job = 3
+                            af_po = data['cube_histories'][i]['after_potential_options']
+                            for l in range(0,af_po.size()):
+                                escape = 1
+                                for x in range(0,eu[job][j].size()):
+                                    if af_po[l]['value'].find(eu[job][j][x]) :
+                                        escape = 0
+                                        break
+                                if escape : break
+                                if l == 2 :
+                                    es[k][j] += 1
         if data['next_cursor'] == '':
             break
         else :
@@ -69,4 +104,13 @@ for i in range(0,5):
             print(100-fun.cdf(s[j][i])*100)
             tsum += t[j][i]
             result += (100-fun.cdf(s[j][i])*100)*t[j][i]
-print(result/tsum)
+result2 = 0
+sum = 0
+for i in range(0,1):
+    for j in range(0,len(e)):
+        if et[i][j] > 0:
+            sum+=et[i][j]
+            fun=stats.norm(ep[i][j]*et[i][j]/100,(ep[i][j]*et[i][j]/100*(1-ep[i][j]/100))**(1/2))
+            result2 += (100-fun.cdf(es[i][j])*100)*et[i][j]
+if tsum > 0 : print(result/tsum)
+if sum > 0 : print(result2/sum)
